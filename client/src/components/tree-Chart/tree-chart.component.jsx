017305bddca030c87e3f0ffc4bdf6e457cc01a04
CptSpooky <legacyofthedonut@gmail.com>
2021-06-16 00:18:54 -0400

    treeview modifications css

--- client/src/components/tree-Chart/tree-chart.component.jsx
+++ client/src/components/tree-Chart/tree-chart.component.jsx
@@ -14,6 +14,7 @@ const useStyles = makeStyles({
     height: "auto",
     flexGrow: 1,
     margin: "1em",
+    color: "white"
   },
 });
 
@@ -52,7 +53,7 @@ const TreeChart = ({data, selectedId, selected}) => {
       defaultExpandIcon={<ChevronRightIcon />}
       selected={selected}
     >
-        <TreeItem label={<div className="d-flex"><MainIcon className={iconClasses.root} />Main</div>} nodeId="main">
+        <TreeItem className="tree-chart" label={<div className="d-flex"><MainIcon className={iconClasses.root} />Main</div>} nodeId="main">
             {data.map((val, key) => {
             return (
                 <TreeItem key={key} nodeId={"" + val.id} label={<div className="d-flex"><Dice className={iconClasses.root}/>{val.name}</div>} onClick={()=> selectedId(val.id)}>
