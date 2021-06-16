e999df9e2f38e75af229f181d78f71a9567a88a7
CptSpooky <legacyofthedonut@gmail.com>
2021-06-16 08:16:20 -0400

    readme

--- README.md
+++ README.md
@@ -1,70 +1,28 @@
-# Getting Started with Create React App
+# Factory Node Tree
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+ ## Table of Contents
+  - [Description](#Description)
+  - [License](#License)
+  - [Contributing](#Contributing)
+  - [Questions](#Questions)
 
-## Available Scripts
+  ## Description
+  ![badge](https://img.shields.io/badge/License-MIT-yellow.svg)
+  [App Link](https://pure-forest-64023.herokuapp.com/)
+  ![App Img](https://user-images.githubusercontent.com/66426144/98880510-e653be00-2455-11eb-8f51-fab51b3157b4.png)
 
-In the project directory, you can run:
+  This app was created with a ReactJs/Axios frontend and NodeJs/Express/MySql backend. JawsDB was used to deploy the database to Heroku.
+  ![img1](https://user-images.githubusercontent.com/66426144/98880560-04212300-2456-11eb-8ac4-25b487f14f64.png) 
 
-### `npm start`
+  ## License
+  [MIT](https://opensource.org/licenses/MIT)
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+  ## Contributing
+  Contact me at my email or linkedIn, but honestly I don't care do whatever you want just pop me into your credits. 
+  This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+  ## Questions
+  [GitHub: CptSpooky](https://github.com/CptSpooky)
+  [Email: legacyofthedonut@gmail.com](legacyofthedonut@gmail.com)
+  Contact me! Email me or message me on LinkedIn!
--- server/server.js
+++ server/server.js
@@ -10,137 +10,175 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+  app.use(express.static("client/build"));
 }
+
+///////////////////////////////////////////////////////////////////////////
+
+/* Database connection*/
+
+///////////////////////////////////////////////////////////////////////////
 
 let db = null;
-/* Database connection*/
 if (process.env.JAWSDB_URL) {
-    db = mysql.createConnection(process.env.JAWSDB_URL);
+  db = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    db = mysql.createConnection({
-        user: process.env.USER,
-        host: process.env.HOST,
-        password: process.env.PASS,
-        database: process.env.DATABASE
-    });
+  db = mysql.createConnection({
+    user: process.env.USER,
+    host: process.env.HOST,
+    password: process.env.PASS,
+    database: process.env.DATABASE
+  });
 }
 
-/* Create a factory */
-app.post('/create', (req, res) => {
-    const name = req.body.name;
-    const qty = req.body.qty;
-    const max = req.body.max;
-    const min = req.body.min;
-    const gentime = Math.floor(Date.now() / 1000) % 0xffffff;
+/************* ROUTES ****************************************************/
 
-    db.query(
-        'INSERT INTO factories (name, qty, max, min, gentime) VALUES (?,?,?,?,?)', 
-        [name, qty, max, min, gentime], 
-        (err, result) => {
-            if (err) {
-                console.log(err);
-            } else {
-                /* Id of inserted operation */
-                res.send("" + result.insertId);
-            }
-        }
-    );    
+///////////////////////////////////////////////////////////////////////////
+
+/* Create a factory */
+
+///////////////////////////////////////////////////////////////////////////
+
+app.post('/create', (req, res) => {
+  const name = req.body.name;
+  const qty = req.body.qty;
+  const max = req.body.max;
+  const min = req.body.min;
+  const gentime = Math.floor(Date.now() / 1000) % 0xffffff;
+
+  db.query(
+    'INSERT INTO factories (name, qty, max, min, gentime) VALUES (?,?,?,?,?)', 
+    [name, qty, max, min, gentime], 
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        /* Id of inserted operation */
+        res.send("" + result.insertId);
+      }
+    }
+  );    
 });
 
+///////////////////////////////////////////////////////////////////////////
+
 /* Get all factories */
+
+///////////////////////////////////////////////////////////////////////////
+
 app.get('/', (req, res) => {
-    db.query('SELECT * FROM factories', (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    });
+  db.query('SELECT * FROM factories', (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
 /* Get all factories */
 app.get('/factories', (req, res) => {
-    db.query('SELECT * FROM factories', (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    });
+  db.query('SELECT * FROM factories', (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
+
+///////////////////////////////////////////////////////////////////////////
 
 /* Get one factory */
+
+///////////////////////////////////////////////////////////////////////////
+
 app.get('/factories/:id', (req, res) => {
-    db.query('SELECT * FROM factories WHERE id = ?',[req.params.id], (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send(result);
-        }
-    });
+  db.query('SELECT * FROM factories WHERE id = ?',[req.params.id], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
+///////////////////////////////////////////////////////////////////////////
+
 /* Delete factory */
+
+///////////////////////////////////////////////////////////////////////////
+
 app.delete('/factories/:id', (req, res) => {
-    console.log("server delete")
-    console.log("ID: " + req.params.id);
-    console.log("Params: " + JSON.stringify(req.params));
-    db.query('DELETE FROM factories WHERE id = ?',[req.params.id], (err, result) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.send('Deleted successfully');
-        }
-    });
+  console.log("server delete")
+  console.log("ID: " + req.params.id);
+  console.log("Params: " + JSON.stringify(req.params));
+  db.query('DELETE FROM factories WHERE id = ?',[req.params.id], (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send('Deleted successfully');
+    }
+  });
 });
+
+///////////////////////////////////////////////////////////////////////////
+
+/* Update a factory */
+
+///////////////////////////////////////////////////////////////////////////
 
 /* Clamp number between two values for validation */
 const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 
-/* Update a factory */
 app.put('/factories/:id', (req, res) => {
-    const name = req.body.name;
-    const qty = clamp(req.body.qty, 1, 15);
-    const max = clamp(req.body.max, -100000000, 100000000);
-    const min = clamp(req.body.min, -100000000, 100000000);
-    const id = req.params.id;
+  const name = req.body.name;
+  const qty = clamp(req.body.qty, 1, 15);
+  const max = clamp(req.body.max, -100000000, 100000000);
+  const min = clamp(req.body.min, -100000000, 100000000);
+  const id = req.params.id;
 
-    db.query(
-        'UPDATE factories SET name = ?, qty = ?, max = ?, min = ? WHERE id = ?', 
-        [name, qty, max, min, id], 
-        (err, result) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.send("Values updated");
-            }
-        }
-    );    
+  db.query(
+    'UPDATE factories SET name = ?, qty = ?, max = ?, min = ? WHERE id = ?', 
+    [name, qty, max, min, id], 
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values updated");
+      }
+    }
+  );    
 });
 
-/* Regenerate factory children */
+///////////////////////////////////////////////////////////////////////////
+
+/* Regenerate random numbers */
+
+///////////////////////////////////////////////////////////////////////////
+
 app.put('/factories/regen/:id', (req, res) => {
-    /* Create seed for random number generation*/
-    const gentime = Math.floor(Date.now() / 1000) % 0xffffff;
-    const id = req.params.id;
+  /* Create seed for random number generation*/
+  const gentime = Math.floor(Date.now() / 1000) % 0xffffff;
+  const id = req.params.id;
 
-
-    db.query(
-        'UPDATE factories SET gentime = ? WHERE id = ?', 
-        [gentime, id], 
-        (err, result) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.send("Values updated");
-            }
-        }
-    );    
+  db.query(
+    'UPDATE factories SET gentime = ? WHERE id = ?', 
+    [gentime, id], 
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("Values updated");
+      }
+    }
+  );    
 });
+
+///////////////////////////////////////////////////////////////////////////
 
 db.connect();
 
 app.listen(PORT, () => {
-    console.log(`server is running on ${PORT}`);
+  console.log(`server is running on ${PORT}`);
 });
